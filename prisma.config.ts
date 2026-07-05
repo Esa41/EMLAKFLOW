37431492f9f1482f7463e55fc15710c6dfa48e73
Esa41 <46722309+Esa41@users.noreply.github.com>
2026-07-06 01:54:33 +0300

    prisma.config.ts: earlyAccess kaldırıldı

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,9 @@ import path from 'node:path'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
   seed: {
     provider: 'tsx',
     args: [path.join(__dirname, 'prisma', 'seed.ts')],
   },
 })
+
